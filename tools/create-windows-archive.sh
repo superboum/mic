4cc7cf45a2ac9a9f9d7fc826186c2013cadcc571
Quentin Dufour <quentin@dufour.tk>
2017-10-14 22:46:54 +0200

    Refactor vendors

--- tools/create-windows-archive.sh
+++ tools/create-windows-archive.sh
@@ -6,6 +6,7 @@ RELEASE=$CURRENT_FOLDER/../Mic/bin/Release
 pushd $RELEASE
 mkdir -p mic-data/lib
 mv *.dll mic-data/lib
-cp ../../../vendors/* mic-data/lib
+cp ../../../vendors/lib/* mic-data/lib
+cp -r ../../../vendors/share mic-data
 zip -r mic-windows-portable.zip .
 popd
--- tools/update-gtk.sh
+++ tools/update-gtk.sh
@@ -3,9 +3,17 @@ TARGET="$(dirname $0)/gtk-working-directory"
 
 mkdir -p $TARGET
 pushd $TARGET
+
+rm -r ./../../vendors
+mkdir -p ./../../vendors/{lib,share}
+
 wget https://dl.xamarin.com/GTKforWindows/Windows/gtk-sharp-2.12.45.msi
 msiextract *.msi
-cp Program\ Files/GtkSharp/2.12/bin/*.dll ./../../vendors/
+
+cp Program\ Files/GtkSharp/2.12/bin/*.dll ./../../vendors/lib
+cp -r Program\ Files/GtkSharp/2.12/lib/gtk-2.0 ./../../vendors/lib
+cp -r Program\ Files/GtkSharp/2.12/share/themes ./../../vendors/share
+
 ls -lah
 popd
 
