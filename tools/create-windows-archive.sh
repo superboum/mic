0dbdc6758382006e27a55736fa01331a08dd4df2
Quentin Dufour <quentin@dufour.tk>
2017-10-14 22:54:18 +0200

    Fix recursive copy errors in vendors

--- tools/create-windows-archive.sh
+++ tools/create-windows-archive.sh
@@ -4,9 +4,11 @@ CURRENT_FOLDER=$(dirname $0)
 RELEASE=$CURRENT_FOLDER/../Mic/bin/Release
 
 pushd $RELEASE
-mkdir -p mic-data/lib
-mv *.dll mic-data/lib
-cp ../../../vendors/lib/* mic-data/lib
+mkdir -p mic-data
+
+cp -r ../../../vendors/lib mic-data
 cp -r ../../../vendors/share mic-data
+mv *.dll mic-data/lib
+
 zip -r mic-windows-portable.zip .
 popd
